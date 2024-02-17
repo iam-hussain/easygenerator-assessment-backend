c65243f4d21e499832983d820c0786aba3d3eaea
Zakir Hussain <itsjakirhussain@gmail.com>
2024-02-17 23:43:02 +0530

    add app guard

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AuthService } from './auth.service';
+import { APP_GUARD } from '@nestjs/core';
 import { UsersModule } from '../users/users.module';
 import { JwtModule } from '@nestjs/jwt';
 import { AuthController } from './auth.controller';
+import { AuthGuard } from './auth.guard';
+import { AuthService } from './auth.service';
 import { jwtConstants } from './constants';
 
 @Module({
@@ -14,7 +16,13 @@ import { jwtConstants } from './constants';
       signOptions: { expiresIn: '7200s' },
     }),
   ],
-  providers: [AuthService],
+  providers: [
+    AuthService,
+    {
+      provide: APP_GUARD,
+      useClass: AuthGuard,
+    },
+  ],
   controllers: [AuthController],
   exports: [AuthService],
 })
